c1ce958dfde3078627f77b02885990edd7d631ac
Carita <carita.shemeikka@hotmail.com>
2021-07-15 10:02:26 +0000

    Modify user repository to use generic repository base

--- TeamRedBackEnd/Database/Models/User.cs
+++ TeamRedBackEnd/Database/Models/User.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace TeamRedBackEnd.Database.Models
 {
-    public class User
+    public class User : IModelBase
     {
         public int Id { get; set; }
         public string Name { get; set; }
